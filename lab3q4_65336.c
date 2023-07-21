b03a170b590142ee07375e759d901e67b9c9b04d
NongFeen <feen@DESKTOP-EC23DAK>
2023-07-21 14:44:42 +0700

    fix line swap

--- lab3q4_65336.c
+++ lab3q4_65336.c
@@ -4,9 +4,10 @@ int main(){
 	do {
 		printf("please input number : ");
 		scanf("%d",&i);
-		sum +=i;
-		if(i>0)
+		if(i>0){
+			sum +=i;
 			count++;
+		}
 	} while (i > 0);
 	printf("sum = %d avg = %f ",sum,(0.0+sum)/count);
 	return 0;
