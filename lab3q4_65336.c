74ce3b6855ce0d7508901e84e1da9533eab5a55f
NongFeen <feen@DESKTOP-EC23DAK>
2023-07-21 14:41:12 +0700

    add avg

--- lab3q4_65336.c
+++ lab3q4_65336.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main(){
-	int i,sum =0;
+	int i,sum,count =0;
 	do {
-        	printf("please input number : ");
-                scanf("%d",&i);
-                sum += i;
-    	} while (i > 0);
-	printf("sum = %d\n",sum);
+		printf("please input number : ");
+		scanf("%d",&i);
+		sum +=i;
+		if(i>0)
+			count++;
+	} while (i > 0);
+	printf("sum = %d avg = %f ",sum,(0.0+sum)/count);
 	return 0;
 }
